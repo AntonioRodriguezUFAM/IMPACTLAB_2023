550fd92850fc567f5e6588ab1b7d1f092065d3ea
AntonioRodriguezUFAM <antonio.rodriguez@icom.ufam.edu.br>
2023-04-02 11:50:55 -0400

    Add project files.

---
+++ IMPACTLAB_2023/Cplus_Aula_01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main() {
+
+	std::cout << std::endl;
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+	std::cout << "-------------------------- IMPACT LAB 2023 ---------------------------" << std::endl;
+	std::cout << "--------------------------  ICOMP - UFAM   ---------------------------" << std::endl;
+	std::cout << "--------------------------   TURMA 2023    ---------------------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "--------------------------   Programação em C++  ---------------------" << std::endl;
+
+
+	std::cout << "                     Prog. Antonio Souto Rodriguez                    " << std::endl;
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+	std::cout << std::endl;
+	return 0;
+
+}
+
